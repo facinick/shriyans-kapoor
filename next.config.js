c247339d116e8fecc3eab8f2a5d20bf91cbc3192
facinick <facinick@gmail.com>
2023-10-12 04:57:11 +0530

    path changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/*": ["./public/pagination.json"],
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
