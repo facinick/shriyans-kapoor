9405696c82b48b6b60c674e4c5727fd7eeb1af18
facinick <facinick@gmail.com>
2024-03-09 00:06:03 +0530

    e

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+  experimental: {
+    outputFileTracingIncludes: {
+      "/*": ["./pagination/**/*.json"],
+      "/*": ["./content/**/*.mdx"],
+      "/*": ["pagination/**/*.json"],
+      "/*": ["content/**/*.mdx"],
+      "/*": ["**/*.json"],
+      "/*": ["**/*.mdx"],
+    },
+  },
 };
 
 module.exports = nextConfig;
