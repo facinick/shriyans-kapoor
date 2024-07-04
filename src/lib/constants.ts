84a35ac53378c93467455461a2400128e21ad890
facinick <facinick@gmail.com>
2024-07-04 05:36:25 +0530

    added authors timezone

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,11 @@
 import { Theme } from "@/types/Theme";
 
 export const APP_TITLE = "@facinick";
+export const APP_TIMEZONE = "Asia/Calcutta";
 export const APP_DESCRIPTION = "Record of stuff.";
 export const APP_THEMES: Theme[] = ["blue", "orange", "black"];
 export const POSTS_DIRECTORY = "/posts";
-export const PAGINATION_DIRECTORY = '/pagination';
+export const PAGINATION_DIRECTORY = "/pagination";
 export const PAGINATION_READ_PATH = `/pagination/pagination.json`;
 export const PAGINATION_WRITE_PATH = `/pagination/pagination.json`;
 export const POSTS_PER_PAGE = 4;
@@ -21,10 +22,10 @@ export const PROD_HOST = `facinick.xyz`;
 export const PROD_PROTOCOL = "https";
 export const PROD_APP_SITE_URL = `${PROD_PROTOCOL}://${PROD_HOST}`;
 // DISQUS
-export const DISQUS_SHORT_NAME = `https://https-facinick-xyz`
+export const DISQUS_SHORT_NAME = `https://https-facinick-xyz`;
 
 export const SOCIALS = {
   telegram: "https://telegram.me/facinick",
   github: "https://github.com/facinick",
-  gmail: "mailto:facinick@gmail.com?subject=Hello"
-}
+  gmail: "mailto:facinick@gmail.com?subject=Hello",
+};
