4220e85369bf81fdc5fc3babc407d053a5a6d4b5
facinick <facinick@gmail.com>
2024-08-25 20:56:29 +0530

    hooks shouldn't be used conditionally

--- src/lib/hooks/useRouter.tsx
+++ src/lib/hooks/useRouter.tsx
@@ -2,7 +2,11 @@ import { useRouter as useNextRouter } from 'next/navigation';
 import { useTransitionRouter } from 'next-view-transitions';
 
 function useRouter() {
-    return globalThis.document ? document.startViewTransition ? useTransitionRouter() : useNextRouter() : null
+
+    const transitionRouter = useTransitionRouter()
+    const nextRouter = useNextRouter()
+
+    return globalThis.document ? document.startViewTransition ? transitionRouter : nextRouter : null
 }
 
 export {
