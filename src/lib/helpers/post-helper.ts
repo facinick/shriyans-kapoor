7ccc3d778d72b36f4205f37f6e103bac041b1d0a
facinick <facinick@gmail.com>
2024-10-11 03:28:03 +0530

    Fault tolerant apis

--- src/lib/helpers/post-helper.ts
+++ src/lib/helpers/post-helper.ts
@@ -1,4 +1,5 @@
 import { Post, PostContent, PostMetadata } from '@/types/Post';
+import { Dirent } from 'fs';
 import { readdir } from 'fs/promises';
 import matter from 'gray-matter';
 import path from 'path';
@@ -9,10 +10,8 @@ import {
   POSTS_PER_PAGE
 } from '../constants';
 import {
-  FileError,
-  getErrorMessage,
   readDirectory,
-  readFile,
+  readFile
 } from './file-helper';
 
 // server side caching
@@ -37,37 +36,54 @@ type PaginatedPostsCacheValue = {
     totalPages: number;
   };
 };
-// Create the caches with the appropriate types
+type PostsCacheValue = {
+  data: PostCacheValue[];
+  pagination: {
+    totalResults: number;
+  };
+};
+
 const categoryCache: Map<string, CategoryCacheValue> = new Map();
 const postCache: Map<string, PostCacheValue> = new Map();
 const postsCache: Map<string, PaginatedPostsCacheValue> = new Map();
 
-export async function getPosts({
-  page,
-  category = CATEGORY_ALL,
-}: {
-  page: number;
-  category?: string;
-}) {
+export async function getPosts(category: string, page: number): Promise<PaginatedPostsCacheValue> {
+
   const cacheKey = `${category}-${page}`;
 
+  // Check if the result is already in the cache
   if (postsCache.has(cacheKey)) {
-    return postsCache.get(cacheKey) as unknown as PaginatedPostsCacheValue
+    return postsCache.get(cacheKey) as unknown as PaginatedPostsCacheValue;
   }
 
-  try {
-    const categories = category !== CATEGORY_ALL
-      ? [category]
-      : (await getCategories())!.data;
+  const categories = category !== CATEGORY_ALL
+    ? [category]
+    : (await getCategories()).data;
 
-    const blogPosts: Array<z.infer<typeof Post>> = [];
+  const blogPosts: Array<z.infer<typeof Post>> = [];
 
-    for (let category of categories) {
-      const categoryDirectory = `${POSTS_DIRECTORY}/${category}`;
-      const fileNames = await readDirectory(categoryDirectory);
+  for (let category of categories) {
+    const categoryDirectory = `${POSTS_DIRECTORY}/${category}`;
+    let fileNames: string[] = [];
 
-      for (let fileName of fileNames) {
-        const rawContent = await readFile(`${categoryDirectory}/${fileName}`);
+    try {
+      fileNames = await readDirectory(categoryDirectory);
+    } catch (error) {
+      console.error(`Error reading directory: ${categoryDirectory}`, error);
+      continue;
+    }
+
+    for (let fileName of fileNames) {
+      let rawContent = '';
+
+      try {
+        rawContent = await readFile(`${categoryDirectory}/${fileName}`);
+      } catch (error) {
+        console.error(`Error reading file: ${fileName}`, error);
+        continue; // skip this file if there's an error
+      }
+
+      try {
         const { data: metadata, content } = matter(rawContent);
 
         blogPosts.push({
@@ -76,64 +92,60 @@ export async function getPosts({
           category,
           content: PostContent.parse(content),
         });
+      } catch (error) {
+        console.error(`Error parsing post: ${fileName}`, error);
+        continue;
       }
     }
-
-    // Sort posts by publishedOn date in descending order
-    blogPosts.sort((post1, post2) =>
-      post2.metadata.publishedOn.localeCompare(post1.metadata.publishedOn)
-    );
-
-    const totalResults = blogPosts.length;
-
-    // if (!page) {
-    //   const result = {
-    //     data: blogPosts,
-    //     pagination: {
-    //       totalResults,
-    //     },
-    //   };
-
-    //   // Cache the result without pagination
-    //   postsCache.set(cacheKey, result);
-
-    //   return result;
-    // }
-
-    // Calculate pagination only if a page is provided
-    const startIndex = (page - 1) * POSTS_PER_PAGE;
-    const endIndex = startIndex + POSTS_PER_PAGE;
-
-    const paginatedPosts = blogPosts.slice(startIndex, endIndex);
-    const totalPages = Math.ceil(totalResults / POSTS_PER_PAGE);
-
-    const result = {
-      data: paginatedPosts,
-      pagination: {
-        currentPage: page,
-        totalResults,
-        pageSize: POSTS_PER_PAGE,
-        totalPages,
-      },
-    };
-
-    // Cache the paginated result
-    postsCache.set(cacheKey, result);
-
-    return result;
-  } catch (error) {
-    handleIOError(error);
   }
+
+  // Sort posts by publishedOn date in descending order
+  blogPosts.sort((post1, post2) =>
+    post2.metadata.publishedOn.localeCompare(post1.metadata.publishedOn)
+  );
+
+  const totalResults = blogPosts.length;
+
+  const startIndex = (page - 1) * POSTS_PER_PAGE;
+  const endIndex = startIndex + POSTS_PER_PAGE;
+
+  const paginatedPosts = blogPosts.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(totalResults / POSTS_PER_PAGE);
+
+  const result = {
+    data: paginatedPosts,
+    pagination: {
+      currentPage: page,
+      totalResults,
+      pageSize: POSTS_PER_PAGE,
+      totalPages,
+    },
+  };
+
+  // Cache the result
+  postsCache.set(cacheKey, result);
+
+  return result;
 }
-export async function getPost({ category, slug }: { slug: string; category: string }) {
+export async function getPost(category: string, slug: string): Promise<PostCacheValue | null> {
+
   const cacheKey = `${category}-${slug}`;
 
+  // Check if the post is already in the cache
   if (postCache.has(cacheKey)) {
-    return postCache.get(cacheKey) as unknown as PostCacheValue
+    return postCache.get(cacheKey) as PostCacheValue;
+  }
+
+  let rawMetaAndContent = '';
+
+  try {
+    rawMetaAndContent = await readFile(`${POSTS_DIRECTORY}/${category}/${slug}.mdx`);
+  } catch (error) {
+    console.error(`Error reading post file: ${slug}.mdx in ${category}`, error);
+    return null;
   }
 
   try {
-    const rawMetaAndContent = await readFile(`${POSTS_DIRECTORY}/${category}/${slug}.mdx`);
     const { data: metadata, content } = matter(rawMetaAndContent);
 
     const post = {
@@ -148,94 +160,106 @@ export async function getPost({ category, slug }: { slug: string; category: stri
 
     return post;
   } catch (error) {
-    handleIOError(error);
+    console.error(`Error parsing content for post: ${slug}`, error);
+    return null;
   }
 }
-function handleIOError(error: unknown) {
-  if (error instanceof Error) {
-    throw error as FileError;
-  } else {
-    const fileError: FileError = {
-      message: getErrorMessage(error),
-    };
-    throw fileError;
-  }
-}
+export async function getCategories(): Promise<CategoryCacheValue> {
 
-// build time helpers
-export async function getCategories() {
-  if (categoryCache.has('categories')) {
-    return categoryCache.get('categories') as unknown as CategoryCacheValue
+  const cacheKey = 'categories';
+
+  // Check if categories are already in the cache
+  if (categoryCache.has(cacheKey)) {
+    return categoryCache.get(cacheKey) as unknown as CategoryCacheValue;
   }
 
   const catoriesDirectory = path.join('.', `${POSTS_DIRECTORY}`);
+  let fileLike: Dirent[];
+
   try {
-    const fileLike = await readdir(catoriesDirectory, { withFileTypes: true });
-    const directories = fileLike.filter(file => file.isDirectory()).map(file => file.name);
-
-    const result: CategoryCacheValue = {
-      data: directories,
-      pagination: {
-        totalResults: directories.length
-      }
-    };
-
-    // Cache categories
-    categoryCache.set('categories', result);
-
-    return result;
+    fileLike = await readdir(catoriesDirectory, { withFileTypes: true });
   } catch (error) {
-    handleIOError(error);
+    console.error(error);
+    fileLike = [];
   }
+
+  const directories = fileLike
+    .filter(file => file.isDirectory())
+    .map(file => file.name);
+
+  const result = {
+    data: directories,
+    pagination: {
+      totalResults: directories.length,
+    },
+  };
+  // Cache the result
+  categoryCache.set(cacheKey, result);
+
+  return result;
 }
+export async function getAllPosts(): Promise<PostsCacheValue> {
 
-export async function getAllPosts(): Promise<{
-  data: Array<z.infer<typeof Post>>;
-  totalResults: number;
-} | undefined> {
+  console.log(`DEBUG: getAllPosts`)
 
-  try {
-    // Fetch all categories
-    const categories = (await getCategories())!.data;
+  const categories = (await getCategories()).data
 
-    const allPosts: Array<z.infer<typeof Post>> = [];
+  const allPosts: Array<z.infer<typeof Post>> = [];
 
-    // Loop through each category and fetch posts
-    for (let category of categories) {
-      const categoryDirectory = `${POSTS_DIRECTORY}/${category}`;
-      const fileNames = await readDirectory(categoryDirectory);
+  // Loop through each category and fetch posts
+  for (let category of categories) {
 
-      for (let fileName of fileNames) {
-        const rawContent = await readFile(`${categoryDirectory}/${fileName}`);
+    const categoryDirectory = `${POSTS_DIRECTORY}/${category}`;
+
+    let fileNames: string[] = []
+
+    try {
+      fileNames = await readDirectory(categoryDirectory);
+    } catch (error) {
+      console.error(error)
+      continue // skip this category
+    }
+
+    for (let fileName of fileNames) {
+
+      let rawContent: string
+
+      try {
+        rawContent = await readFile(`${categoryDirectory}/${fileName}`);
+      } catch (error) {
+        console.error(error)
+        continue // skip this file
+      }
+
+      try {
         const { data: metadata, content } = matter(rawContent);
-
         allPosts.push({
           slug: fileName.replace('.mdx', ''),
           metadata: PostMetadata.parse(metadata),
           category,
           content: PostContent.parse(content),
         });
+      } catch (error) {
+        console.error(`error parsing rawContent: ${rawContent}`, error)
       }
     }
-
-    // Sort posts by publishedOn date in descending order
-    allPosts.sort((post1, post2) =>
-      post2.metadata.publishedOn.localeCompare(post1.metadata.publishedOn)
-    );
-
-    const totalResults = allPosts.length;
-
-    const result = {
-      data: allPosts,
-      totalResults,
-    };
-
-    return result;
-  } catch (error) {
-    handleIOError(error);
   }
-}
 
+  allPosts.sort((post1, post2) =>
+    post2.metadata.publishedOn.localeCompare(post1.metadata.publishedOn)
+  );
+
+  const totalResults = allPosts.length;
+
+  const result = {
+    data: allPosts,
+    pagination: {
+      totalResults
+    },
+  };
+
+  return result;
+}
 export {
   type CategoryCacheValue, type PaginatedPostsCacheValue, type PostCacheValue
 };
