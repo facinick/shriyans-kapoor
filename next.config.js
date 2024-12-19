3a82a9ace68d3c91c17179337b21de4a639328ac
Shriyans Kapoor <skapoor@junipersquare.com>
2024-12-19 18:35:12 +0530

    feat: no redirects, serve redirected page duplicate from home

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/', // The root URL
-        destination: '/all?page=1', // Redirect to this URL
-        permanent: false, // Set to `false` for a 302 permanent redirect
-      },
-    ];
-  },
   compiler: {
     removeConsole: false,
   },
